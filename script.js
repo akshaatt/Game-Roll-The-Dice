2bede4c5adc20887e9699ff9d10513d89aa66e49
hawkeyepirate <lordhawkeye25@gmail.com>
2021-05-11 23:28:20 +0530

    xyz

--- script.js
+++ script.js
@@ -7,14 +7,17 @@ const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
 let player0El = document.getElementById('player--0');
 let player1El = document.getElementById('player--1');
 
 score0El.textContent = 0;
-score1El.textContent = '0';
+score1El.textContent = 0;
 diceEl.classList.add('hidden');
+const score = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let holdScore = 0;
 // Rolling The Dice Functionality
 
 btnRoll.addEventListener('click', function () {
@@ -39,4 +42,8 @@ btnRoll.addEventListener('click', function () {
     activePlayer = activePlayer === 0 ? 1 : 0;
     currentScore = 0;
   }
+});
+btnHold.addEventListener('click', function () {
+  //Adding Current Score
+  score[activePlayer] += currentPlayer;
 });
